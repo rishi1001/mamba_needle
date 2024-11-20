03bd04b6aee5ba37e22f8aa9177ea11b01e80f9b
rishi1001 <rishi.shah10122001@gmail.com>
2024-11-20 18:35:50 -0500

    added nn_mamba.py

--- python/needle/nn/nn_conv.py
+++ python/needle/nn/nn_conv.py
@@ -35,3 +35,30 @@ class Conv(Module):
         ### BEGIN YOUR SOLUTION
         raise NotImplementedError()
         ### END YOUR SOLUTION
+
+class Conv1d(Module):
+    """
+    Multi-channel 1D convolutional layer
+    IMPORTANT: Accepts inputs in NCHW format, outputs also in NCHW format
+    Only supports padding=same
+    No grouped convolution or dilation
+    """
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, bias=True, device=None, dtype="float32"):
+        super().__init__()
+        if isinstance(kernel_size, tuple):
+            kernel_size = kernel_size[0]
+        if isinstance(stride, tuple):
+            stride = stride[0]
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+        ### BEGIN YOUR SOLUTION
+        raise NotImplementedError()
+        ### END YOUR SOLUTION
+
+    def forward(self, x: Tensor) -> Tensor:
+        ### BEGIN YOUR SOLUTION
+        raise NotImplementedError()
+        ### END YOURSOLUTION
