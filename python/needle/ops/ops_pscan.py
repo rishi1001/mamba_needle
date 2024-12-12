77f637f567fe8a36cde05afb23b64665cec217fc
Saketram Durbha <durbhasaketram@gmail.com>
2024-12-11 19:59:30 -0500

    comments updates

--- python/needle/ops/ops_pscan.py
+++ python/needle/ops/ops_pscan.py
@@ -1,6 +1,4 @@
-"""Operator implementations."""
-
-from typing import Tuple
+"""Parallel scan operationS implementation."""
 
 from ..autograd import NDArray, TensorOp, Value
 from .ops_mathematic import *
@@ -8,10 +6,23 @@ from .ops_tuple import *
 
 
 class PScan(TensorOp):
+    """Parallel scan operation.
+
+    Takes two input 4D matrices A and X of size (B, D, L, N) and computes the parallel scan
+    operation on them:
+
+    result[:, :, i, :] = A[:, :, i, :] * result[:, :, i - 1, :]  + X[:, :, i, :]
+
+    The forward pass is mostly just a thin wrapper around the CUDA `pscan` code, which handles the
+    computation of the result.
+
+    Note that both input matrices are required to live on a CUDA device. If not, a sequential scan
+    — not this class — should be used.
+    """
 
     def compute(self, A: NDArray, X: NDArray):  # type: ignore
-        # A : (B, L, D, N)
-        # X : (B, L, D, N)
+        # A: (B, L, D, N)
+        # X: (B, L, D, N)
 
         # the pscan operation requires the input to be in the form of (B, D, L, N)
         A = A.compact().permute((0, 2, 1, 3)).compact()  # (B, D, L, N)
@@ -21,9 +32,17 @@ class PScan(TensorOp):
         return result.permute((0, 2, 1, 3))
 
     def gradient(self, out_grad: Value, node: Value):
-        # A_in : (B, L, D, N)
-        # X_in : (B, L, D, N)
-        # out_grad : (B, L, D, N)
+        """The partial gradient of the parallel scan operation.
+
+        It computes a "reverese pscan" operation, where each element of the result is a sum of all
+        the elements that come ahead of it and itself.
+
+        This is adapted from the https://github.com/alxndrTL/mamba.py repository.
+        """
+
+        # A_in: (B, L, D, N)
+        # X_in: (B, L, D, N)
+        # out_grad: (B, L, D, N)
         # node: (B, L, D, N)
         A_in, X_in = node.inputs
 
@@ -51,7 +70,7 @@ class ReversePScan(TensorOp):
         A = A[:, :, 1:, :].pad(((0, 0), (0, 0), (0, 1), (0, 0)))
         return A.compact().reverse_pscan(X.compact())
 
-    def gradient(self, out_grad: Value, node: Value) -> Tuple[Value, ...]:
+    def gradient(self, out_grad: Value, node: Value):
         # not needed for our purposes
         raise NotImplementedError
 
